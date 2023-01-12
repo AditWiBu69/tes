666ff43cc7a70dac1c57533853bd71c1f2bd1a2b
AditWiBu69 <adityafirmansya69@gmail.com>
2023-01-12 09:06:23 +0700

    db

---
+++ --.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>News</title>
+</head>
+<body>
+<div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="INDvsVIE.jpg" height="400" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Sports</h5>
+        <p class="card-text">Leg pertama semifinal Kejuaraan AFF 2022 akan berlangsung pada Jumat di Stadion Gelora Bung Karno di Jakarta antara Indonesia dan Vietnam. Dengan 10 poin dari empat pertandingannya, Tim Garuda menempati posisi kedua di Grup A, tertinggal dari Thailand dalam selisih gol.
+Tim Shin Tae-yong menang tiga kali, termasuk mengalahkan Brunei 7-0, dan bermain imbang sekali untuk melaju ke semifinal kompetisi untuk kedua kalinya berturut-turut. Dengan 10 poin dari empat pertandingan, Vietnam menjuarai Grup B. Mereka belum menyerah satu gol pun, dan sejauh ini mereka mencatatkan clean sheet di setiap pertandingan mereka.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="Jokowi.jpg" height="400" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Indonesia</h5>
+        <p class="card-text">Presiden Joko "Jokowi" Widodo mengatakan dia mungkin merombak kabinetnya dalam beberapa hari ke depan, karena beberapa sumber politik bertaruh pada penataan kembali pemerintahannya untuk menyelesaikan program menjelang pemilu yang akan diadakan pada tahun 2024. Artikel ini diterbitkan di thejakartapost .com dengan judul "Presiden Jokowi mengincar perombakan kabinet beberapa hari mendatang". Namun, ujian sesungguhnya bagi kemampuan raksasa Asia Tenggara itu untuk menangani konflik ada di hadapannya sendiri saat Indonesia memimpin ASEAN tahun ini.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="Kpop.jpeg" height="400" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Entertaiment</h5>
+        <p class="card-text">Rookie girl group NewJeans has achieved a new personal record with their new mini-album OMG. According to an update by Hanteo Chart, OMG sold more than 480,000 copies on its first day of sales alone, surpassing the first week sales of the girl group’s eponymous debut album.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="Myanmar.jpg" height="400" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">World</h5>
+        <p class="card-text">Middle power Indonesia may now have earned the trust of the international community as a “neutral” peace mediator for its successful management of the Group of 20, in which leaders unanimously condemned Russia’s brutal invasion of Ukraine without appearing to take sides in the conflict.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+
+<div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="Ronaldo.jpg" height="400" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Sports</h5>
+        <p class="card-text">Cristiano Ronaldo telah resmi diperkenalkan sebagai penyerang baru Al Nassr.
+Acara presentasi Cristiano Ronaldo berlangsung di Stadion Mrsool Park, Riyadh, Selasa (3/1/2023) malam WIB.
+Usai seremoni, CR7 langsung mengikuti latihan bersama rekan setim yang disaksikan puluhan ribu penonton yang memenuhi stadion.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
